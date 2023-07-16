09b031b0b7062c2f42d7fe1c4ba552a9f6771c4a
stefania11 <druga.stefania@gmail.com>
2023-07-15 18:41:06 -0700

    fix diffusion

--- pages/index.tsx
+++ pages/index.tsx
@@ -343,7 +343,7 @@ export default function Home() {
             )}
           </Box>
         )}
-        {messagesArray.length === 3 && (
+        {/* {messagesArray.length === 3 && ( */}
           <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
             {/* <div className="relative py-3 sm:max-w-xl sm:mx-auto">
               <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 via-green-500 to-cyan-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
@@ -399,7 +399,8 @@ export default function Home() {
               }
             `}</style>
           </div>
-        )}
+          {/* )} */}
+
       </Container>
       <Container size="sm">
         <Center style={{ height: 200 }}>
